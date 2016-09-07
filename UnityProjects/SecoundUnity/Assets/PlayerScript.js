1deea2a6e2cbfd82c72e97002acd159df1c6f46f
kishila <s16g459@stu.kagawa-u.ac.jp>
2016-09-07 13:36:08 +0900

    lesson#14

--- UnityProjects/SecoundUnity/Assets/PlayerScript.js
+++ UnityProjects/SecoundUnity/Assets/PlayerScript.js
@@ -1,9 +1,13 @@
 ﻿#pragma strict
 
-function Start () {
+var x : int = 5;
 
+function Start () {
+	Debug.Log("hello world -> " + x);
 }
 
 function Update () {
-
+	//transform.position.z += 0.1;
+	//transform.position.z += Vector3(0, 0, 0.1);
+	transform.Translate(0, 0, 0.1);
 }
