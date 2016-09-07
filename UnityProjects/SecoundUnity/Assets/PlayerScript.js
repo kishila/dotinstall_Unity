0f78d2b4d1c842661aed0f485a1de4710ed81a3e
kishila <s16g459@stu.kagawa-u.ac.jp>
2016-09-07 13:50:53 +0900

    lesson#16

--- UnityProjects/SecoundUnity/Assets/PlayerScript.js
+++ UnityProjects/SecoundUnity/Assets/PlayerScript.js
@@ -7,7 +7,17 @@ function Start () {
 }
 
 function Update () {
-	//transform.position.z += 0.1;
-	//transform.position.z += Vector3(0, 0, 0.1);
-	transform.Translate(0, 0, 0.1);
+	if (Input.GetButtonUp("Jump")) {
+		Debug.Log("jumped!");
+	}
+
+	var x : float = Input.GetAxis("Horizontal");
+	transform.Translate(x * 0.2, 0, 0);
+	
+}
+
+function OnCollisionEnter (obj : Collision) {
+	if (obj.gameObject.name == "RightWall") {
+		Debug.Log("Hit RightWall!");
+	}
 }
